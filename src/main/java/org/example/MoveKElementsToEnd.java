ee0abb271db7b2d76b3a78f6c7f7f8f11648b272
RagYt <ragyt91@gmail.com>
2023-10-24 13:40:47 +0530

    MoveKElementsToEnd

---
+++ src/main/java/org/example/MoveKElementsToEnd.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class MoveKElementsToEnd
+{
+    public static void main( String[] args )
+    {
+        int[] arrNew = { 1, 1, 3, 5, 6,1 };
+        moveElementsToEnd(arrNew, 1);
+    }
+    public static void moveElementsToEnd(int[] arr, int toMove){
+        int i=0;
+        int j=arr.length-1;
+        while (i<j){
+            while (i<j && arr[j] == toMove)
+                j--;//to decrement if last index already contains toMove element
+            if(arr[i]==toMove){
+                int tmp = arr[i];
+                arr[i]= arr[j];
+                arr[j]=tmp;
+            }
+            i++;
+        }
+        for(Integer q: arr)
+            System.out.print(q+",");
+    }
+}
